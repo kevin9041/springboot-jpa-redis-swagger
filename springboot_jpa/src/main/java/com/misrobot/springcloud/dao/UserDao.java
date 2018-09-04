f33e9e8288b15b346e1b2fff706611695bf9d4fa
weihuanbo <22@qq.com>
2018-09-04 14:09:33 +0800

    somechange

--- springboot_jpa/src/main/java/com/misrobot/springcloud/dao/UserDao.java
+++ springboot_jpa/src/main/java/com/misrobot/springcloud/dao/UserDao.java
@@ -3,7 +3,6 @@ package com.misrobot.springcloud.dao;
 import com.misrobot.springcloud.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
 public interface UserDao extends JpaRepository<User, Long>{
 
 	User findByUsername(String username);
